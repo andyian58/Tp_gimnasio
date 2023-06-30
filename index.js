c2413ddcfeaa9c2f617e5c3e0f38fe87917eabab
andyian58 <andres.anokian@gmail.com>
2023-06-30 14:08:18 -0300

    Agrego aca index.js

---
+++ index.js
@@ -0,0 +1,19 @@
+var cambiodom=`<nav class="navppal">
+<a class="headnav" href="index.html">Home</a>
+<a class="headnav" href="about.html">Sobre nosotros</a>
+<a class="headnav" href="contact.html">Contacto</a>
+<a class="headnav" href="form.html">Registro</a>
+<a class="headnav" href="location.html">Sucursales</a>
+<a class="headnav" href="gallery.html">Galería de fotos</a>
+
+</nav>`
+document.getElementById("idheader").innerHTML=cambiodom
+
+var cambiodom=`<footer class="footer">Todos los derechos reservados @2023 Antonio Montalto, Andres Anokian, Sebastian<br><br>
+
+<a href="https://www.facebook.com"><i class="fa-brands fa-facebook fa-xl"></i></a>
+<a href="https://www.twitter.com"><i class="fa-brands fa-square-twitter fa-xl" style="color: #000fdf;"></i>
+    <a href="https://www.instagram.com"><i class="fa-brands fa-square-instagram fa-xl"
+            style="color: #000fdf;"></i>
+</footer>`
+document.getElementById("idfooter").innerHTML=cambiodom
